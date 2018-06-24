58239c9c04d73891e0b92bf499170b9ec2ec1805
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-06-24 03:34:14 -0300

    Instalando NODEMON monitoramento de codigo
    NODEMON é usado para ficar monitorando nosso codigo em tempo real, ajuda
    muito por questão de nao ficarmos exercutando todo momento o SERVE:3000

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 let app = express();
 
+
+// DEFINIR variaveis para dentro do express
+app.set('view engine', 'ejs');
+
 // configurando a ROTA '/produtos' e QUANDO...
 app.get('/produtos', function(req, res){
-    res.send("<html><body><h1>Listagem de produtos</h1></body></html>");
+    console.log("listandoo..");
+    
+    res.render("produtos/lista"); // pasta/arquivo
 });
 
-// Listen: chmando o servidor e QUANDO estiver PRONTO vai ter tal ação
+
+/////////// Listen: chamando o servidor e QUANDO estiver PRONTO vai ter tal ação
 app.listen(3000, function(){
     console.log('servidor está rodando');
     
